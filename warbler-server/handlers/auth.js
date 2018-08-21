574872829e5d059e0790d7ea9c80fa5bfeead6dd
wolf637 <alexey.volkov@live.com>
2018-08-21 16:08:43 -0700

    Add signin route

--- warbler-server/handlers/auth.js
+++ warbler-server/handlers/auth.js
@@ -2,11 +2,38 @@ const db = require("../models");
 const jwt = require("jsonwebtoken");
 
 
-exports.signing = function(){}
+exports.signing = function(req, res, next){
+	try {
+		let user = await db.User.findOne({
+		email: req.body.email
+	})
+	let { id, username, profileImageUrl } = user;
+	let isMatch = await user.comparePassword(req.body.password);
+	if(isMatch){
+		let token = jwt.sign({
+			id,
+			username,
+			profileImageUrl
+		}, process.env.SECRET_KEY)
+		return res.status(200).json({
+			id,
+			username,
+			profileImageUrl,
+			token
+		})
+	} else {
+		return next({
+			status: 400,
+			message: "Invalid Email and/or Password"
+		})
+	}
+	} catch(e) {
+		return next({status: 400, message: "Invalid Email and/or Password"})
+	}
+}
 
 exports.signup = async function(req, res, next){
 	try {
-		// create user
 		let user = await db.User.create(req.body);
 		let {id, username, profileImageUrl} = user
 		let token = jwt.sign({
@@ -16,7 +43,6 @@ exports.signup = async function(req, res, next){
 		},
 		process.env.SECRET_KEY);
 
-		// create token (signing a token)
 		return res.status(200).json({
 			id, 
 			username, 
@@ -24,15 +50,12 @@ exports.signup = async function(req, res, next){
 		})
 	} catch(err) {
 		if(err.code === 11000){
-			err.message = "Sorry, that username and/or email is taken"
+			err.message = "Sorry, that username and/or email is taken";
 		}
-		// see what kind of error
-
-		// if it is a certain error
-
-		// respond with username/email already taken
-
-		// otherwise just sent back generic 400
+		return next({
+			status: 400,
+			message: err.message
+		})
 
 	}
 }
