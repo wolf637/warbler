9a5d8e8e21764bec59c7a890a1edc635cf824836
wolf637 <alexey.volkov@live.com>
2018-08-20 21:54:39 -0700

    Add User model

---
+++ warbler-server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	profileImageurl: {
+		type: String
+	}
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User
