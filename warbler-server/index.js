819b894f217041fb4d9cca6efc911be1681cc237
wolf637 <alexey.volkov@live.com>
2018-08-22 20:40:06 -0700

    Add initial messages setup

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,29 +1,28 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const errorHandler = require("./handlers/error")
-const authRoutes = require("./routes/auth")
-
+const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const db = require("./models");
 const PORT = 8081;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", messagesRoutes);
 
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
-app.use(function(req, res, next){
-	let err = new Error("Not Found")
-	err.status = 404
-	next(err)
-})
+app.use(errorHandler);
 
-
-app.use(errorHandler)
-
-app.listen(PORT, function(){
-	console.log(`Server is starting on port ${PORT}`)
-})
+app.listen(PORT, function() {
+  console.log(`Server is starting on port ${PORT}`);
+});
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
+ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
-	keepAlive: true
-})
+mongoose.connect("mongodb://localhost:27017/warbler", {
+  keepAlive: true,
+  useNewUrlParser: true 
+});
 
-module.exports.User = require("./user")
-module.exports.Message = require("./message")
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
