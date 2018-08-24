2a5c20d5a3b294b47615643b3ba05a637222e673
wolf637 <alexey.volkov@live.com>
2018-08-23 21:04:00 -0700

    Setup Redux store

--- warbler-client/src/store/reducers/currentUser.js
+++ warbler-client/src/store/reducers/currentUser.js
@@ -5,11 +5,14 @@ const DEFAULT_STATE = {
 	user: {}
 }
 
-export default (state = DEFAULT_STATE, action => {
+export default (state = DEFAULT_STATE, action) => {
 	switch (action.type){
 		case SET_CURRENT_USER:
 			return {
-				isAuthenticated: Object.keys(action.user).length > 0
+				isAuthenticated: Object.keys(action.user).length > 0,
+				user: action.user
 			}
+		default:
+			return state
 	}
-})
+}
