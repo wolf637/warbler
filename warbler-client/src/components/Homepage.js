93513b1b38f44fc5094a989add8d8c289bfd9b3b
wolf637 <alexey.volkov@live.com>
2018-08-23 22:27:36 -0700

    Add Homepage JSX component; apply stylings on Homepage component

---
+++ warbler-client/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Homepage = () => (
+		<div className="home-hero">
+			<h1>What's Happening?</h1>
+			<h4>New to Warbler?</h4>
+			<Link to="/signup" className="btn btn-primary">Signup Here</Link>
+		</div>
+	)
+
+export default Homepage;
